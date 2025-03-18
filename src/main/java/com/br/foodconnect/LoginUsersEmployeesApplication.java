a3c3cfbbb7f5f9da0be51a22173e4d415836c8a8
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-03-18 00:39:23 -0300

    first commit

---
+++ src/main/java/com/br/foodconnect/LoginUsersEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.br.foodconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUsersEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginUsersEmployeesApplication.class, args);
+	}
+
+}
