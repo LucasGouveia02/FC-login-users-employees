6d1416c977c1ce3346e6518e2cea51215b4aa26e
GabrielLomba12 <lombagabriel324@gmail.com>
2025-03-23 19:38:23 -0300

    fix: a caminhorquivo .env c e crossOrigin adicionado

--- src/main/java/com/br/foodconnect/controller/CustomerController.java
+++ src/main/java/com/br/foodconnect/controller/CustomerController.java
@@ -4,11 +4,10 @@ import com.br.foodconnect.dto.LoginDTO;
 import com.br.foodconnect.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 public class CustomerController {
 
     @Autowired
